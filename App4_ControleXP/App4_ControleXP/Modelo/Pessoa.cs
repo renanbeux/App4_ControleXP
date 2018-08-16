d7b912ce442c261a43ef3e1934efca92906536c8
Renan Beux <nanerxueb@hotmail.com>
2018-08-16 16:18:57 -0300

    16/08/18 16:18

---
+++ App4_ControleXP/App4_ControleXP/Modelo/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App4_ControleXP.Modelo
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+    }
+}
